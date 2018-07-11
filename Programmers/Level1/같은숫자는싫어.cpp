c583c6801b2498435600739b78c3d913dbb262b9
yjw5344 <yjw5344@hanmail.net>
2018-07-12 00:49:33 +0900

    Memo : 2018.07.12

--- Programmers/Level1/같은숫자는싫어.cpp
+++ Programmers/Level1/같은숫자는싫어.cpp
@@ -7,20 +7,15 @@ vector<int> solution(vector<int> arr)
 {
     vector<int> answer;
 
-    for(int i = 0 ; i < arr.size() ; i++){
-      for(int j = i+1 ; j< arr.size() ; j++){
-        if(arr[i] == arr[j]){
-          continue;
-        }else{
-          answer.push_back(arr[i]);
-          i = j;
-          break;
-        }
-      }
-      if( i == arr.size()-1){
+    for(int i = 0 ; i < arr.size()-1 ; i ++){
+      if(i == 0 ){
         answer.push_back(arr[i]);
       }
-
+      
+      if( arr[i] != arr[i+1]){
+        answer.push_back(arr[i+1]);
+      }
     }
+
     return answer;
 }
