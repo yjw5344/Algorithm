12dc1491146b11edbddd588353ec9ca02848cda0
yjw5344 <yjw5344@hanmail.net>
2018-07-15 16:01:36 +0900

    Date		 : "2018.07.15" Memo	 : 소수찾기문제

--- test.cpp
+++ test.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
-#include<string>
 #include<vector>
 
 using namespace std;
 
 int main(){
 
-  string answer = "";
-  vector<string> result;
-  result.push_back("Jane");
-  result.push_back("Kim");
+  int n = 10;
+  vector<bool> arr;
+  arr.resize(n);
 
-  for(int i = 0 ; i < result.size(); i++){
-    if(result[i] == "Kim"){
-      answer += "김서방은 ";
-      answer += to_string(i);
-      answer += "에 있다";
-    }
+  for(int i = 0;i<n;i++){
+    arr[i] = i;
   }
-  cout << answer << endl;
+
+  cout << " ========= " << endl;
+  cout << arr[1] << endl;
+  cout << arr.at(0) << endl;
+  cout << arr.at(1) << endl;
+
+
+
+
   return 0;
 }
