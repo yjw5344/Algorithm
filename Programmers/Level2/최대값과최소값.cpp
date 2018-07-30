6ff3ebdf67cba8ae9f652a23b2fbeb36b244baab
yjw5344 <yjw5344@hanmail.net>
2018-07-30 15:32:49 +0900

    Date		: 2018.07.30 Memo	: Make Level2

---
+++ Programmers/Level2/최대값과최소값.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    return answer;
+}
--- test.cpp
+++ test.cpp
@@ -1,36 +1,7 @@
 #include<iostream>
-#include<string>
-#include<vector>
 
 using namespace std;
 
 int main(){
-  vector<string> tmp;
-
-  tmp.push_back("show");
-  tmp.push_back("me");
-  tmp.push_back("the");
-  tmp.push_back("money");
-
-
-  cout << tmp[0] << endl;
-  cout << tmp[1] << endl;
-  cout << tmp[2] << endl;
-  cout << tmp[3] << endl;
-  cout << tmp.size() << endl;
-  cout << tmp.at(0)[0] << endl;
-  cout << tmp[0][1] << endl;
-  cout << tmp[0][2] << endl;
-
-  // for(int i=0; i< tmp.size() ; i++){
-  //   for(int j = 0 ; tmp[i].length() ; j++){
-  //     if(j == 0 ){
-  //       cout << tmp[i].at(j) << endl;
-  //     }
-  //   }
-  // }
-
-
-
   return 0;
 }
