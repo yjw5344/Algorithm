4e606fa89de3bd2442a590ec811b637be37786be
yjw5344 <yjw5344@hanmail.net>
2018-08-08 14:25:56 +0900

    Date		: 2018.08.08 Memo	: 임시저장

---
+++ Programmers/Level2/땅따먹기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int> > land){
+
+  int answer = 0;
+  int prePosition = -1;
+
+
+  for(int i = 0 ; i < land.size() ; i++){
+    
+
+  }
+
+
+
+
+
+
+  return answer;
+}
--- test.cpp
+++ test.cpp
@@ -1,15 +1,23 @@
-// 이차원 vector 연습
-
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
-  vector< vector<int> > arr1;
+  vector<int> arr;
 
+  arr.push_back(1);
+  arr.push_back(12);
+  arr.push_back(13);
+  arr.push_back(4);
+  arr.push_back(5);
+  arr.push_back(6);
 
-  arr1.
+  int n1 = 10;
+  int n2 = 9;
+  cout << max(arr.begin(), arr.end()) << endl;
+
 
 
   return 0;
