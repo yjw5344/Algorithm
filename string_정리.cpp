1b825313c80a8f08d10030947ad16756c61654dd
yjw5344 <yjw5344@hanmail.net>
2018-09-22 23:40:05 +0900

    Date		: 2018.09.22 Memo	: 커밋2

--- string_정리.cpp
+++ string_정리.cpp
@@ -1,4 +1,4 @@
-#include <iosteam>
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -7,10 +7,10 @@ int main(){
 
   string str = "hello world";
 
-  string tmp = str.substring(0,5);
-  string tmp1 = str.substring(6,10);
+  string tmp = str.substr(0,5);
+  string tmp1 = str.substr(6,3);
 
   cout << tmp << endl;
-  cout << tmp1 << end;
+  cout << tmp1 << endl;
   return 0;
 }
