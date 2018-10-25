80775e223616d600de7e8e2f43cf42f0b48be616
yjw5344 <yjw5344@hanmail.net>
2018-10-25 21:26:16 +0900

    Date		: 2018.10.25 Memo	: 기능개발 완료!

--- Programmers/Level2/기능개발.cpp
+++ Programmers/Level2/기능개발.cpp
@@ -1,9 +1,33 @@
-#include <string>
+#include <queue>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
-    return answer;
+  queue<int> work;
+	vector<int> answer;
+	for (int i = 0; i < progresses.size() ; i++){
+		int n = 100 - progresses.at(i);
+		work.push(ceil((double)n / speeds.at(i)));
+	}
+
+	int cur = work.front();
+	int n = 1;
+	work.pop();
+	while (!work.empty()){
+		if (work.front() <= cur){
+			work.pop();
+			n++;
+		}
+		else{
+			answer.push_back(n);
+			cur = work.front();
+			work.pop();
+			n = 1;
+		}
+	}
+	answer.push_back(n);
+
+	return answer;
 }
