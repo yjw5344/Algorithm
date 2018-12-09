f5e6518a5587cfb217fe8b35a9db326dd3de2e71
yjw5344 <yjw5344@hanmail.net>
2018-12-09 23:18:03 +0900

    Date		:  2018.12.09 Memo	: 땅따먹기 문제 해결!

--- Programmers/Level2/땅따먹기.cpp
+++ Programmers/Level2/땅따먹기.cpp
@@ -1,34 +1,60 @@
-// 아직 다 못 푼 문제
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int solution(vector<vector<int> > land){
+int solution(vector< vector<int> > land){
 
   int answer = 0;
-  int prePosition = -1;
-  vector<int> result;
+  int line = land.size();
 
-
-  for(int i = 0 ; i < land.size() ; i++){
-    int max = 0 ;
-    int tmpPosition;
-    for(int j = 0 ; j < land.at(i).size(), j++){
-      if(prePosition != j){
-        if(max < land.at(i).at(j)){
-          max = land.at(i).at(j);
-          tmpPosition = j;
+  for(int i = 1 ; i < line ; i++){
+    for(int j = 0 ; j < 4 ; j++){
+      vector<int> maxNum;
+      for(int k = 0 ; k < 4 ; k++){
+        if( j == k)
+          continue;
+        else{
+          maxNum.push_back(land.at(i-1).at(k));
         }
       }
+      int max = *max_element(maxNum.begin(), maxNum.end());
+      land.at(i).at(j) = land.at(i).at(j) + max;
     }
-    prePosition = tmpPosition;
   }
 
-
-
-
-
-
+  vector<int> result;
+  for(int i = 0 ; i < 4 ; i++){
+    result.push_back(land.at(line-1).at(i));
+  }
+  answer = *max_element(result.begin(), result.end());
   return answer;
+}
+
+int main(){
+  vector< vector<int> > arr;
+  vector<int> row;
+  row.push_back(1);
+  row.push_back(2);
+  row.push_back(3);
+  row.push_back(5);
+  arr.push_back(row);
+
+
+  row.clear();
+  row.push_back(5);
+  row.push_back(6);
+  row.push_back(7);
+  row.push_back(8);
+  arr.push_back(row);
+
+  row.clear();
+  row.push_back(4);
+  row.push_back(3);
+  row.push_back(2);
+  row.push_back(1);
+  arr.push_back(row);
+
+  cout << solution(arr) << endl;
+  return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-
-class A{
-public:
-  int a;
-};
-
-
 int main(){
-  A tmp;
-  tmp.a = 10;
 
-  cout << tmp.a << endl;
+  vector<int> arr;
+  arr.push_back(10);
+  arr.push_back(15);
+  arr.push_back(12);
+  arr.push_back(111);
+  arr.push_back(17);
+  arr.push_back(156);
+  //
+  // int max = *max_element(arr.begin(), arr.end());
+  // cout << max << endl;
+
+  // int num = *max_element(arr.at(0), arr.at(1), arr.at(2));
+  // cout << max << endl;
+
+
+
+  return 0;
 }
