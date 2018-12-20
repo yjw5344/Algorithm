86057d138cf218a3714a93ca03b7af5a30420b0c
yjw5344 <yjw5344@hanmail.net>
2018-12-20 21:27:09 +0900

    Date		: 2018.12.12 MEmo	: 우선순위큐 업데이트

---
+++ [0] 자주사용하는 함수 정리/자료형/priority_queue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int main(){
+   // 우선순위 큐
+   // 우선순위로 큐를 정렬해준다.
+   // priority_queue<int, vector<int>, greater<int>> pq; => 기본 형태이다.
+
+   priority_queue< pair < int, int >, vector< pair < int, int > >, less< pair < int, int > > > pq;
+
+   // pair의 첫번째 인자의 우선순위 순서
+   // pair의 첫번째 인자의 우선순위가 같은 경우, 두번째 인자의 우선순위로 판단한다.
+
+   pq.push(make_pair(4, 11));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+   pq.push(make_pair(5, 5));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+   pq.push(make_pair(5, 6));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+   pq.push(make_pair(5, 3));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+   pq.push(make_pair(7, 3));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+   pq.push(make_pair(9, 1));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+   pq.push(make_pair(8, 2));
+   cout << pq.top().first << ", " << pq.top().second << endl;
+
+   return 0;
+}
