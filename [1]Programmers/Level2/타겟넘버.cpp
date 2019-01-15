5f769b8033c22475c84123aad52bd7fef878f0f3
yjw5344 <yjw5344@hanmail.net>
2019-01-15 16:06:21 +0900

    Date			: 2019.01.15 Memo		:  프로그래머스 타겟넘버 완료!

--- [1]Programmers/Level2/타겟넘버.cpp
+++ [1]Programmers/Level2/타겟넘버.cpp
@@ -4,34 +4,43 @@
 using namespace std;
 
 int answer = 0;
+void dfs(vector<int> numbers, int target, int depth ){
+  int numbersLen = numbers.size();
+  if(depth == numbersLen){
+    int sum = 0;
+    for(int i = 0 ; i < numbersLen ; i++){
+      sum += numbers.at(i);
+    }
+    if(sum == target){
+      answer++;
+    }
+  }else{
+    // 인덱스의 숫자가 양수인 경우
+    numbers.at(depth) = numbers.at(depth) * 1;
+    dfs(numbers, target, depth+1);
 
-void dfs(vector<int> numbers, int target, int k ){
-  int len = numbers.size();
-
-
-
-
+    // 인덱스의 숫자가 음수인 경우
+    numbers.at(depth) = numbers.at(depth) * (-1);
+    dfs(numbers, target, depth+1);
+  }
 }
-
 
 int solution(vector<int> numbers, int target) {
   dfs(numbers, target, 0);
-
-
-    return answer;
+  return answer;
 }
 
 int main(){
-  vector<int> arr;
+  vector<int> testArr;
   int target = 3;
 
-  arr.push_back(1);
-  arr.push_back(1);
-  arr.push_back(1);
-  arr.push_back(1);
-  arr.push_back(1);
+  testArr.push_back(1);
+  testArr.push_back(1);
+  testArr.push_back(1);
+  testArr.push_back(1);
+  testArr.push_back(1);
 
-  cout << solution(arr, target) << endl;
+  cout << solution(testArr, target) << endl;
 
   return 0;
 }
