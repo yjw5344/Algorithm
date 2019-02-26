a0240f50837f4f1742c1b4dab49f5a2f3f930f90
yjw5344 <yjw5344@hanmail.net>
2019-02-26 18:41:53 +0900

    Date		: 2019.02.26 Memo	: 소수찾기문제완료!

--- Java_Algorithm/src/프로그래머스/Level2/소수찾기.java
+++ Java_Algorithm/src/프로그래머스/Level2/소수찾기.java
@@ -1,25 +1,77 @@
 package 프로그래머스.Level2;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 class Solution_소수찾기 {
+    ArrayList<Integer> numberList = new ArrayList<Integer>();
+
     public int solution(String numbers) {
-        String[] stringNumberList = numbers.split("");
-        String[] targetNumberList;
-
-
-
-
-
-
-
-
-
+        int numberMaxLength = numbers.length();
+        int depth = 0;
         int answer = 0;
+        String[] stringNumberList = numbers.split("");
+
+        for(int r = 1 ; r <= numberMaxLength; r++){
+            String[] tempList = new String[r];
+            perm(stringNumberList, depth, numberMaxLength, r, tempList);
+        }
+
+        for(int i : numberList){
+            boolean primeCheck = true;
+            for(int j = 2 ; j < i ; j++){
+                if( i % j == 0){
+                    primeCheck = false;
+                    break;
+                }
+            }
+            if(primeCheck){
+                answer++;
+            }
+        }
         return answer;
     }
-}
 
+    public void perm(String[] stringNumberList, int depth, int length,int r, String[] tempList){
+
+        if (depth == r) {
+            makeNumber(tempList);
+            return;
+        }
+
+        for (int i = depth; i < length; i++) {
+            swap(stringNumberList, i, depth);
+            tempList[depth] = stringNumberList[depth];
+            perm(stringNumberList, depth + 1, length, r,tempList);
+            swap(stringNumberList, i, depth);
+        }
+
+    }
+
+    public void swap(String[] stringNumberList, int i, int depth){
+        String temp = stringNumberList[i];
+        stringNumberList[i] = stringNumberList[depth];
+        stringNumberList[depth] = temp;
+    }
+
+    public void makeNumber(String[] tempList){
+        String number = "";
+        for(int i = 0 ; i < tempList.length ; i++){
+            number += tempList[i];
+        }
+        addNumber(number);
+    }
+
+    public void addNumber(String number){
+        int num = Integer.parseInt(number);
+        if(num == 0 || num == 1){
+            return;
+        }
+
+        if(!(numberList.contains(num))){
+            numberList.add(num);
+        }
+    }
+}
 
 
 public class 소수찾기 {
@@ -28,7 +80,7 @@ public class 소수찾기 {
         String number1 = "17";
         String number2 = "011";
 
-        System.out.println(number1 + " : " + s.solution(number1));
+        //System.out.println(number1 + " : " + s.solution(number1));
         System.out.println(number2 + " : " + s.solution(number2));
 
     }
