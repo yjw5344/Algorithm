13baa452b66c58109a9ec4b0efae1d6bf3681654
yjw5344 <yjw5344@hanmail.net>
2019-04-22 21:40:53 +0900

    큰수만들기 Stack Version Update

--- Java_Algorithm/src/프로그래머스/Level2/큰수만들기.java
+++ Java_Algorithm/src/프로그래머스/Level2/큰수만들기.java
@@ -1,7 +1,11 @@
 package 프로그래머스.Level2;
 
+import java.util.Stack;
+
 class Solution_큰수만들기 {
-    public String solution(String number, int k) {
+
+    public String solution1(String number, int k) {
+        // for문을 이용하는 방법
         int index = 0;
         char max;
         StringBuilder answer = new StringBuilder();
@@ -19,6 +23,41 @@ class Solution_큰수만들기 {
 
         return answer.toString();
     }
+
+    public String solution2(String number, int k) {
+        // Stack을 이용하는 방법
+
+        int size = number.length()-k;
+        StringBuffer str = new StringBuffer();
+//        char[] result = new char[number.length()-k];
+
+        Stack<Character> st = new Stack<Character>();
+        for(int i = 0 ; i < number.length(); i++){
+            char num = number.charAt(i);
+
+            while(!st.isEmpty()){
+                if(st.peek() < num && k > 0){
+                    st.pop();
+                    k--;
+                }else{
+                    break;
+                }
+            }
+            st.push(num);
+        }
+
+//        for(int i = 0 ; i < result.length ; i++){
+//            result[i] = st.get(i);
+//        }
+//        return new String(result);
+
+
+        for(int i = 0 ; i < size; i++){
+            str.append(st.get(i));
+        }
+        return str.toString();
+
+    }
 }
 
 public class 큰수만들기 {
@@ -32,9 +71,12 @@ public class 큰수만들기 {
         int k2 = 3;
         int k3 = 4;
 
-        System.out.println(s.solution(number1, k1));
-        System.out.println(s.solution(number2, k2));
-        System.out.println(s.solution(number3, k3));
+        System.out.println(s.solution1(number1, k1));
+        System.out.println(s.solution1(number2, k2));
+        System.out.println(s.solution1(number3, k3));
+        System.out.println(s.solution2("10000", 2));
+        System.out.println(s.solution2(number2, k2));
+        System.out.println(s.solution2(number3, k3));
 
     }
 }
