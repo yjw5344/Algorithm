39803428cd0f1c615b9ea9abc15f2b0372209298
yjw5344 <yjw5344@hanmail.net>
2022-04-17 00:17:00 +0900

    JavaScript 프로그래머스 완주하지못한선수 추가

--- JavaScript/[1]Programmers/완주하지못한선수.js
+++ JavaScript/[1]Programmers/완주하지못한선수.js
@@ -1,12 +1,36 @@
-function solution(participant, completion) {
-    var answer = '';
+const solution = (participant, completion) => {
+
+    let map = new Map();
+    let answer = "";
+
+    for(let people of participant) {
+        if(map.get(people)){
+            map.set(people,map.get(people)+1);
+        } else {
+            map.set(people,1);
+        }
+    }
+
+    for(let person of completion) {
+        if(map.get(person)) {
+            map.set(person, map.get(person)-1);
+        }
+    }
+
+    for(let people of participant){
+        if(map.get(people) === 1) {
+            answer = people;
+        }
+    }
+
     return answer;
 }
 
+// const participant =® ["l®eo", "kiki", "eden"];
+// const completion = ["eden", "kiki"];
 
-const sol = () => {
-  return 1;
-}
+const participant = ["mislav", "stanko", "mislav", "ana"];
+const completion = ["stanko", "ana", "mislav"];
 
-console.log(sol());
-ㅠㅠㅠ
+console.log(solution(participant, completion));
+
