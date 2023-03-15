3e9a0af132f78ff02781436db188dd6b4b5df976
yjw5344 <yjw5344@hanmail.net>
2023-03-15 16:23:58 +0900

    다리를지나는트럭.js 추가

--- JavaScript/[1]Programmers/코딩테스트 고득점 Kit/다리를 지나는 트럭.js
+++ JavaScript/[1]Programmers/코딩테스트 고득점 Kit/다리를 지나는 트럭.js
@@ -1,4 +1,27 @@
 function solution(bridge_length, weight, truck_weights) {
+
+    let onBridge = [];
+
+    while (truck_weights.bridge_length > 0) {
+        let popTruck = truck_weights.shift();
+
+
+    }
+
     var answer = 0;
     return answer;
 }
+
+let bridge_length1 = 2;
+let weight1 = 10;
+let truck_weights1 = [7,4,5,6];
+
+let bridge_length2 = 100;
+let weight2 = 100;
+let truck_weights2 = [10];
+
+let bridge_length3 = 100;
+let weight3 = 100;
+let truck_weights3 = [10,10,10,10,10,10,10,10,10,10];
+
+console.log(solution(bridge_length1, weight1, truck_weights1));
